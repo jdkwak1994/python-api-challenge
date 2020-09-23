2e00224604ce83cd8c415ac53ab3f7d2be1420cb
jdkwak1994 <jdkwak1994@gmail.com>
2020-09-23 01:34:51 -0400

    google api key changed

--- api_keys.py
+++ api_keys.py
@@ -2,4 +2,6 @@
 weather_api_key = "b5777e93d849c1d1d05bdd34d4358036"
 
 # Google API Key
+# this key is invalid now because I changed the key
+# I didnt want my key to be in public
 g_key = "AIzaSyBHAzsSXEZiyereP3utSy7hRpzi2iT5V1k"
